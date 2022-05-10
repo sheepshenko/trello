9fec7310560122e79bd0eb77a1799e21c7fcf67d
Sheepshenko <sheepshenko@gmail.com>
2022-05-10 12:15:21 -0500

    Configuración de script parar crear tarjetas

--- app.js
+++ app.js
@@ -3,3 +3,18 @@ require('dotenv').config();
 if(!process.env.TOKEN && !process.env.KEY){
     throw new Error('No hay configuración con  API key y con Token');
 }
+
+let Trello = require("trello");
+let trello = new Trello(process.env.KEY, process.env.TOKEN);
+
+let cardTitle = `Card Nueva ${new Date()}`
+
+trello.addCard(cardTitle, "LaunchX Card Description", "627a8c8bf3abe40356ee49ed",
+	function (error, trelloCard) {
+		if (error) {
+			console.log('Could not add card:', error);
+		}
+		else {
+			console.log('Added card:', trelloCard);
+		}
+	});
